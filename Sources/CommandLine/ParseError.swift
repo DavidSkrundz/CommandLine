295a7b6b17167e446d94e770c046fc681a08d044
David Skrundz <david@skrundz.ca>
2017-11-05 16:36:54 -0700

    Let errors be representable as strings

--- Sources/CommandLine/ParseError.swift
+++ Sources/CommandLine/ParseError.swift
@@ -3,13 +3,24 @@
 //  CommandLine
 //
 
-public enum ParseError: Error {
-	/// Flag, Argument, Expected
-	case invalidArgument(String, String, String)
+public enum ParseError: Error, CustomStringConvertible {
+	/// Flag
+	case invalidFlag(String)
 	
 	/// Flag, Expected
 	case missingArgument(String, String)
 	
-	/// Flag
-	case invalidFlag(String)
+	/// Flag, Argument, Expected
+	case invalidArgument(String, String, String)
+	
+	public var description: String {
+		switch self {
+			case let .invalidFlag(f):
+				return "Invalid flag: \(f)"
+			case let .missingArgument(f, e):
+				return "Missing argument for \(f). Expecting: \(e)"
+			case let .invalidArgument(f, a, e):
+				return "Invalid argument for \(f). Got \(a), Expecting: \(e)"
+		}
+	}
 }
